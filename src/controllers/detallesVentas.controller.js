39bee1b586f0eb6ca583866cfb61cfce5c1b9ebb
Kenneth Parrales <kenneth.parrales@gmail.com>
2024-02-17 15:18:51 -0600

    Prints

--- src/controllers/detallesVentas.controller.js
+++ src/controllers/detallesVentas.controller.js
@@ -77,33 +77,29 @@ export const printDetallesVenta = async (req, res) => {
       return res.status(404).json({ message: 'Detalles de ventas no encontrados' });
     }
 
-
     const configuracion = await Configuracion.findOne(); 
 
-
-    if (!detallesVenta || !configuracion) {
-      return res.status(404).json({ message: 'Data not found' });
+    if (!configuracion) {
+      return res.status(404).json({ message: 'Configuración no encontrada' });
     }
 
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({
+      executablePath: 'C:\\Users\\kenne\\.cache\\puppeteer\\chrome\\win64-119.0.6045.105\\chrome-win64\\chrome.exe',
+      // Otras opciones de configuración según sea necesario
+    });
 
     const page = await browser.newPage();
     
-
-  
     const content = `
     <html>
       <head>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
       </head>
       <body>
         <div style="font-size: 10px; border: 1px solid black; width:280px; text-align:center"> 
-       
-
-          <p style="font-size:23px; text-align:center">Mafy Store <p>
+          <p style="font-size:23px; text-align:center">Mafy Store</p>
           <p style="font-size: 14px;">ID-Factura </br> ${detallesVenta._id}</p>
           <p style="font-size: 12px;">Fecha: ${new Date(detallesVenta.id_ventas.createdAt).toLocaleString('es-ES') } </br>  Cliente: ${detallesVenta.id_ventas.cliente} </p>
-        
           <table style="width:95%; border-collapse: collapse; margin:0 auto">
             <tr>
               <th style=" text-align: left; font-size: 14px;">Artículos</th>
@@ -120,29 +116,27 @@ export const printDetallesVenta = async (req, res) => {
                   ${articulo.id_talla.talla}
                 </td>
                 <td style="border: text-align: left; font-size: 12px;">${articulo.precio.toFixed(2)}</td>
-                <td style="border: text-align: rigth; font-size: 12px;">${articulo.cantidad}</td>
+                <td style="border: text-align: right; font-size: 12px;">${articulo.cantidad}</td>
                 <td style="border: text-align: left; font-size: 12px;">${articulo.subtotal.toFixed(2)}</td>
               </tr>
             `).join('')}
           </table>
-          
           <p style="font-size: 12px;">Total: ${detallesVenta.id_ventas.total.toFixed(2)}</p>
           <p style="font-size: 12px;">Dirección: ${configuracion.direccion}</p>
           <p style="font-size: 12px;">E-Mail: ${configuracion.correo_electronico}</p>
           <p style="font-size: 12px;">Teléfono 1: ${configuracion.telefono_1}  Teléfono 2: ${configuracion.telefono_2}</p>
-            <p style="font-size:12px; text-align:center">Precios incluyen IVA<p>
-          <p style="font-size:23px; text-align:center">¡Gracias por su Compra!<p>
+          <p style="font-size:12px; text-align:center">Precios incluyen IVA</p>
+          <p style="font-size:23px; text-align:center">¡Gracias por su Compra!</p>
         </div>
       </body>
     </html>
-  `;
+    `;
 
-await page.setContent(content);
-await page.waitForTimeout(1000); 
-const pdfBuffer = await page.pdf();
+    await page.setContent(content);
+    await page.waitForTimeout(1000); 
+    const pdfBuffer = await page.pdf();
 
-await browser.close();
-
+    await browser.close();
 
     res.setHeader('Content-Disposition', `attachment; filename=detallesVenta_${id}.pdf`);
     res.setHeader('Content-Type', 'application/pdf');
@@ -150,6 +144,6 @@ await browser.close();
 
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Error generating PDF' });
+    res.status(500).json({ message: 'Error generando el PDF' });
   }
 };
