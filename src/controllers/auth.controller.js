ead6d3d8f3eb696b708ba5a751520aaf7b87dfc8
Kenneth Parrales <kenneth.parrales@gmail.com>
2025-09-14 10:09:15 -0600

    Test

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -5,7 +5,7 @@ import { SECRET } from "../config.js";
 
 export const signupHandler = async (req, res) => {
   try {
-    const { username, email, password, roles } = req.body;
+    const { username, email, password } = req.body;
 
     // Crear un nuevo objeto de usuario
     const newUser = new User({
@@ -14,36 +14,30 @@ export const signupHandler = async (req, res) => {
       password,
     });
 
-    // Verificar roles
-    if (roles) {
-      const foundRoles = await Role.find({ name: { $in: roles } });
-      newUser.roles = foundRoles.map((role) => role._id);
-    } else {
-      const role = await Role.findOne({ name: "user" });
-      newUser.roles = [role._id];
-    }
-
+    // Siempre asigna el rol "user" al crear una cuenta
+    const role = await Role.findOne({ name: "user" });
+    newUser.roles = [role._id];
 
     const savedUser = await newUser.save();
 
-   const token = jwt.sign({ id: savedUser._id }, SECRET, {
-  expiresIn: '1d', 
-});
+    const token = jwt.sign({ id: savedUser._id }, SECRET, {
+      expiresIn: "1d",
+    });
 
     return res.status(200).json({ token, _id: savedUser._id });
   } catch (error) {
-    return res.status(500).json(error.message);
+    return res.status(500).json({ message: error.message });
   }
-};  
+};
 
 export const signinHandler = async (req, res) => {
   try {
- 
     const userFound = await User.findOne({ email: req.body.email }).populate(
       "roles"
     );
 
-    if (!userFound) return res.status(400).json({ message: "Usuario no encontrado" });
+    if (!userFound)
+      return res.status(400).json({ message: "Usuario no encontrado" });
 
     const matchPassword = await User.comparePassword(
       req.body.password,
@@ -56,18 +50,18 @@ export const signinHandler = async (req, res) => {
         message: "Contraseña inválida",
       });
 
- 
     const token = jwt.sign({ id: userFound._id }, SECRET, {
-      expiresIn: '1d', 
+      expiresIn: "1d",
     });
 
     res.json({ token, _id: userFound._id });
   } catch (error) {
     console.log(error);
-    res.status(500).json(error.message);
+    res.status(500).json({ message: error.message });
   }
 };
 
+// Blacklist de tokens en memoria
 const blacklist = new Map();
 
 const invalidateToken = (userId, tokenToInvalidate) => {
@@ -81,34 +75,51 @@ export const logoutHandler = async (req, res) => {
   let tokenToInvalidate;
 
   try {
-    tokenToInvalidate = req.headers['x-access-token'];
+    tokenToInvalidate = req.headers["x-access-token"];
 
     if (!tokenToInvalidate) {
-      return res.status(400).json({ message: "Token not provided in the header" });
+      return res
+        .status(400)
+        .json({ message: "Token not provided in the header" });
     }
 
     const decodedToken = jwt.verify(tokenToInvalidate, SECRET);
     const userId = decodedToken.id;
 
-    if (blacklist.has(userId) && blacklist.get(userId).has(tokenToInvalidate)) {
-      return res.status(401).json({ message: "Token already invalidated, session closed", originalToken: tokenToInvalidate, decodedToken });
+    if (
+      blacklist.has(userId) &&
+      blacklist.get(userId).has(tokenToInvalidate)
+    ) {
+      return res.status(401).json({
+        message: "Token already invalidated, session closed",
+        originalToken: tokenToInvalidate,
+        decodedToken,
+      });
     }
-
-    // Perform additional actions upon logging out if necessary
-    // For example, closing the session in the database for traditional sessions (not applicable to JWTs)
 
     invalidateToken(userId, tokenToInvalidate);
 
-    res.status(200).json({ message: "Session closed successfully", invalidatedToken: tokenToInvalidate, originalToken: tokenToInvalidate, decodedToken, userId });
+    res.status(200).json({
+      message: "Session closed successfully",
+      invalidatedToken: tokenToInvalidate,
+      originalToken: tokenToInvalidate,
+      decodedToken,
+      userId,
+    });
   } catch (error) {
     console.error(error);
 
     if (error instanceof jwt.TokenExpiredError) {
-      return res.status(401).json({ message: "Token expired, session closed", originalToken: tokenToInvalidate });
+      return res.status(401).json({
+        message: "Token expired, session closed",
+        originalToken: tokenToInvalidate,
+      });
     }
 
-    // Handle other types of errors gracefully
-    res.status(500).json({ message: "Error while logging out", originalToken: tokenToInvalidate, error: error.message });
+    res.status(500).json({
+      message: "Error while logging out",
+      originalToken: tokenToInvalidate,
+      error: error.message,
+    });
   }
 };
-
